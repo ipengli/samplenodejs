37f999a277a5f3118e3593e0d3d318c00e113a0b
Peng <peng@Pengs-MacBook-Pro.local>
2019-06-04 17:48:17 +1000

    Add mocha for testing

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World with test!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
